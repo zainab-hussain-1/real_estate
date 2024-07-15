63f82543f113aea148a53ab7e9ad0dba5542dd0e
zainab-hussain-1 <z56670184@gmail.com>
2024-07-15 13:20:19 +0500

    connect to Database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+mongoose
+  .connect("mongodb://localhost:27017/") .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Connection error:", err);
+  });
 
 const app = express();
-app.listen(3000,()=>{
-    console.log("server is running on port 3000")
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
 
-})
+
