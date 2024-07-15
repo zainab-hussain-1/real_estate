5dbc0ecff3260aceb944308995598a842817d1cd
zainab-hussain-1 <z56670184@gmail.com>
2024-07-15 15:00:49 +0500

    create signupapi route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 dotenv.config();
 
 mongoose
@@ -13,6 +14,9 @@ mongoose
   });
 
 const app = express();
+
+
+app.use(express.json());
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
@@ -22,4 +26,6 @@ app.listen(3000, () => {
 
 // routes
 app.use('/api/user',userRouter)
+app.use('/api/auth',authRouter)
+
 
