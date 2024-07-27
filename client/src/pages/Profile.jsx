c359b4228b94c2409cf50cbe8f61db15c19cbfcc
zainab-hussain-1 <z56670184@gmail.com>
2024-07-27 13:40:16 +0500

    complete User Delete functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,9 +8,200 @@
 //       request.resource.contentType.matches('image/.*')
 //     }
 //   }
-// // // }
 
+// import React, { useEffect, useState, useRef } from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import { app } from "../firebase";
+// import {
+//   getDownloadURL,
+//   getStorage,
+//   ref,
+//   uploadBytesResumable,
+// } from "firebase/storage";
+// import {
+//   updateUserStart,
+//   updateUserFailure,
+//   updateUserSuccess,
+//   deleteUserFailure,
+//   deleteUserStart,
+//   deleteUserSuccess
+// } from "../redux/user/userSlice";
 
+// function Profile() {
+//   const fileRef = useRef(null);
+//   const { currentUser, loading, error } = useSelector((state) => state.user);
+//   const [file, setFile] = useState(null);
+//   const [filePerc, setFilePerc] = useState(0);
+//   const [fileUploadError, setFileUploadError] = useState(false);
+//   const [formData, setFormData] = useState({});
+//   const [updateSuccess, setUpdateSuccess] = useState(false);
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     if (file) {
+//       handleFileUpload(file);
+//     }
+//   }, [file]);
+
+//   const handleFileUpload = (file) => {
+//     const storage = getStorage(app);
+//     const fileName = new Date().getTime() + "-" + file.name;
+//     const storageRef = ref(storage, fileName);
+//     const uploadTask = uploadBytesResumable(storageRef, file);
+
+//     uploadTask.on(
+//       "state_changed",
+//       (snapshot) => {
+//         const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+//         setFilePerc(Math.round(progress));
+//       },
+//       (error) => {
+//         setFileUploadError(true);
+//         console.error("Upload failed:", error);
+//       },
+//       () => {
+//         getDownloadURL(uploadTask.snapshot.ref)
+//           .then((downloadURL) => {
+//             setFormData((prevFormData) => ({
+//               ...prevFormData,
+//               avatar: downloadURL,
+//             }));
+//           })
+//           .catch((error) => {
+//             console.error("Error getting download URL:", error);
+//           });
+//       }
+//     );
+//   };
+
+//   const handleChange = (e) => {
+//     setFormData({ ...formData, [e.target.id]: e.target.value });
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     dispatch(updateUserStart());
+//     setUpdateSuccess(false); // Reset success state
+
+//     try {
+//       const res = await fetch(`/api/user/update/${currentUser._id}`, {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(formData),
+//       });
+
+//       const data = await res.json();
+//       if (!res.ok) {
+//         // Handle non-200 responses
+//         const errorMessage = data.message || 'Something went wrong.';
+//         dispatch(updateUserFailure(errorMessage));
+//         return;
+//       }
+
+//       // Update user successfully
+//       dispatch(updateUserSuccess(data));
+//       setUpdateSuccess(true);
+//     } catch (error) {
+//       // Handle errors during fetch
+//       const errorMessage = error.message || 'Unknown error occurred.';
+//       dispatch(updateUserFailure(errorMessage));
+//     }
+//   };
+
+//   const handleDeleteUser = async()=>{
+//     try{
+// dispatch(deleteUserStart());
+// const res = await fetch(`/api/user/delete/${currentUser._id}`,{
+//   method:'DELETE',
+
+// })
+// const data =await res.json();
+// if(data.success === false){
+//   dispatch (deleteUserFailure(data.message));
+//   return;
+// }
+// dispatch (deleteUserSuccess(data));
+//     }catch(error){
+//       dispatch(deleteUserFailureUserFailure(error.message))
+//     }
+//   }
+
+//   return (
+//     <div className="p-3 max-w-lg mx-auto">
+//       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+//       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+//         <input
+//           onChange={(e) => setFile(e.target.files[0])}
+//           type="file"
+//           ref={fileRef}
+//           hidden
+//           accept="image/*"
+//         />
+//         <img
+//           onClick={() => fileRef.current.click()}
+//           src={formData.avatar || currentUser.avatar}
+//           alt="profile"
+//           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+//         />
+//         <p className="text-sm self-center">
+//           {fileUploadError ? (
+//             <span className="text-red-700">Error uploading image</span>
+//           ) : filePerc === 100 ? (
+//             <span className="text-green-700">Image successfully uploaded</span>
+//           ) : filePerc > 0 ? (
+//             <span className="text-teal-700">Uploading: {filePerc}%</span>
+//           ) : (
+//             ""
+//           )}
+//         </p>
+//         <input
+//           type="text"
+//           id="username"
+//           placeholder="username"
+//           defaultValue={currentUser.username}
+//           className="border p-3 rounded-lg"
+//           onChange={handleChange}
+//         />
+//         <input
+//           type="email"
+//           id="email"
+//           placeholder="email"
+//           defaultValue={currentUser.email}
+//           className="border p-3 rounded-lg"
+//           onChange={handleChange}
+//         />
+//         <input
+//           type="password"
+//           id="password"
+//           placeholder="password"
+//           className="border p-3 rounded-lg"
+//         />
+//         <button
+//           type="submit"
+//           disabled={loading}
+//           className="bg-slate-700 text-white rounded-lg p-3 hover:opacity-95 disabled:opacity-80"
+//         >
+//           {loading ? 'Loading...' : 'Update'}
+//         </button>
+//       </form>
+//       <div className="flex justify-between mt-5">
+//         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
+//         <span className="text-red-700 cursor-pointer">Sign out</span>
+//       </div>
+
+//       {/* Display the error if it exists */}
+//       {error && <p className="text-rose-700">{error}</p>}
+//       {/* Display the success message if updateSuccess is true */}
+//       {updateSuccess && (
+//         <p className="text-green-700 mt-5">User updated successfully!</p>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Profile;
 
 import React, { useEffect, useState, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
@@ -24,7 +215,10 @@ import {
 import {
   updateUserStart,
   updateUserFailure,
-  updateUserSuccess
+  updateUserSuccess,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
 } from "../redux/user/userSlice";
 
 function Profile() {
@@ -52,7 +246,8 @@ function Profile() {
     uploadTask.on(
       "state_changed",
       (snapshot) => {
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         setFilePerc(Math.round(progress));
       },
       (error) => {
@@ -85,9 +280,9 @@ function Profile() {
 
     try {
       const res = await fetch(`/api/user/update/${currentUser._id}`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
@@ -95,7 +290,7 @@ function Profile() {
       const data = await res.json();
       if (!res.ok) {
         // Handle non-200 responses
-        const errorMessage = data.message || 'Something went wrong.';
+        const errorMessage = data.message || "Something went wrong.";
         dispatch(updateUserFailure(errorMessage));
         return;
       }
@@ -105,8 +300,34 @@ function Profile() {
       setUpdateSuccess(true);
     } catch (error) {
       // Handle errors during fetch
-      const errorMessage = error.message || 'Unknown error occurred.';
+      const errorMessage = error.message || "Unknown error occurred.";
       dispatch(updateUserFailure(errorMessage));
+    }
+  };
+
+  const handleDeleteUser = async () => {
+    dispatch(deleteUserStart());
+    try {
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+
+      const data = await res.json();
+      if (!res.ok || data.success === false) {
+        // Handle non-200 responses or failure
+        const errorMessage = data.message || "Failed to delete user.";
+        dispatch(deleteUserFailure(errorMessage));
+        console.error("Delete failed:", errorMessage);
+        return;
+      }
+
+      // User deleted successfully
+      dispatch(deleteUserSuccess(data));
+      // Redirect to the sign-up page
+      window.location.href = "/sign-up"; // Redirects to the sign-up page
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+      console.error("Delete failed:", error.message);
     }
   };
 
@@ -165,14 +386,19 @@ function Profile() {
           disabled={loading}
           className="bg-slate-700 text-white rounded-lg p-3 hover:opacity-95 disabled:opacity-80"
         >
-          {loading ? 'Loading...' : 'Update'}
+          {loading ? "Loading..." : "Update"}
         </button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span
+          onClick={handleDeleteUser}
+          className="text-red-700 cursor-pointer"
+        >
+          Delete Account
+        </span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
-      
+
       {/* Display the error if it exists */}
       {error && <p className="text-rose-700">{error}</p>}
       {/* Display the success message if updateSuccess is true */}
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -72,11 +72,25 @@ const userSlice = createSlice({
       state.error=action.payload;
       state.loading=false;
 
-    }
+    },
+    deleteUserStart:(state)=>{
+      state.loading=true;
+    },
+    deleteUserSuccess:(state)=>{
+      state.currentUser = null;
+      state.loading= false;
+      state.error= null;
+    },
+    deleteUserFailure:(state,action)=>{
+      state.error=action.payload;
+      state.loading=false;
+    },
 
   },
 });
 
-export const { signInStart, signInSuccess, signInFailure ,updateUserFailure,updateUserSuccess,updateUserStart} = userSlice.actions;
+export const { signInStart, signInSuccess, 
+  deleteUserFailure,deleteUserSuccess,deleteUserStart,signInFailure ,updateUserFailure,updateUserSuccess,updateUserStart} = userSlice.actions;
 
 export default userSlice.reducer;
+
