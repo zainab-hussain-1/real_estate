cb4b3308809c31cc9d076b80a39711b0962b4a45
zainab-hussain-1 <z56670184@gmail.com>
2024-07-30 11:57:48 +0500

    Complete show user listing

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -39,6 +39,8 @@ function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError,setshowListingsError]=useState(false); 
+  const [userListings,setUserListings]=useState([]);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -160,7 +162,20 @@ const handleSignOutUser = async () => {
     }
   };
   
-
+const handleShowListings=async()=>{
+  try{
+    setshowListingsError(false);
+const res = await fetch(`/api/user/listings/${currentUser._id}`);
+const data=await res.json();
+if(data.success===false){
+  setshowListingsError(true);
+  return;
+}
+setUserListings(data);
+  }catch(error){
+setshowListingsError(true);
+  }
+}
   
   
   return (
@@ -241,7 +256,37 @@ const handleSignOutUser = async () => {
       {updateSuccess && (
         <p className="text-green-700 mt-5">User updated successfully!</p>
       )}
+      <button  onClick={handleShowListings}
+      className="text-green-700 w-full">Show listings</button>
+      <p className="text-red-700 mt-5">{showListingsError ? 'Error showing listing' : ''}</p>
+      
+      
+      {userListings && 
+      userListings.length > 0 && 
+      <div className="flex flex-col gap-4">
+        <h1 className="text-center my-7 text-2xl font-semibold">Your Listings</h1>
+      {userListings.map((listing)=>  <div key={listing._id}
+      className=" border rounded-lg p-3  flex justify-between
+      items-center gap-4"
+        >
+          <Link to={`/listing/${listing._id}`}>
+          <img src={listing.imageUrls[0]} alt="listing 
+          cover"  className="h-16 w-16 object-contain "/>
+          </Link>
+          <Link  className=" text-slate-700   font-semibold  hover:underline truncate flex-1"
+           to={`/listing/${listing._id}`}>
+          <p > {listing.name}</p>
+          </Link>
+          <div className=" flex flex-col items-center">
+<button className="text-red-700 uppercase">Delete</button>
+<button className="text-green-700 uppercase">Edit</button>
+          </div>
+        </div>
+      )}
+      </div>}
+      
     </div>
+
   );
 }
 
